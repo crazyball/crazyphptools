b3d72ba885c175cfa5b6cf5360d86cf37fd8377b
Ambrosini Loïc <crazyball@crazyball.fr>
2013-09-24 11:57:55 +0200

    Initial projet HashMap
    Php Hashmap Class inspirated from the Java Hashmap Class

---
+++ Java to Php/HashMap.php
@@ -0,0 +1,218 @@
+<?php
+
+/**
+ * HashMap
+ * 
+ * Equivalent de JAVA HashMap en PHP
+ * 
+ * @access public
+ * @author Ambrosini Loïc (CraZyBaLL) <loic.ambrosini@me.com>
+ */
+class HashMap implements \Iterator {
+
+    private $_hashTable = array();
+    private $_valid = true;
+
+    /**
+     * Supprime les clef/valeurs de la HashMap
+     * @return void
+     */
+    public function clear() {
+        $this->_hashTable = array();
+    }
+    
+    /**
+     * Retourne 'true' si la map contient la clef spécifiée
+     * @param string $key
+     * @return boolean
+     */
+    public function containsKey($key) {
+        if(!is_scalar($key)) {
+            throw new Exception("La clef de HashMap doit être une donnée scalaire");
+        }
+        if(empty($key)) {
+            throw new Exception("Clef vide passée dans HashMap::containsKey");
+        }
+        return array_key_exists($key, $this->_hashTable);
+    }
+
+    /**
+     * Retourne 'true' si la map contient la valeur spécifiée
+     * @param mixed $value
+     * @return boolean
+     */
+    public function containsValue($value) {
+        return in_array($value, $this->_hashTable);
+    }
+
+    /**
+     * Retourne la valeur de la map pour la clef spécifiée
+     * @param string $key clef
+     * @return mixed valeur
+     */
+    public function get($key) {
+        if(!key_exists($key, $this->_hashTable)) {
+            return NULL;
+        }
+        return $this->_hashTable[$key];
+    }
+
+    /**
+     * Retourne 'true' si la map ne contient aucun mapping clef/valeur
+     * @return boolean
+     */
+    public function isEmpty() {
+        if(count($this->_hashTable) === 0) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+    
+    /**
+     * Retourne un tableau de clefs de la HashMap
+     * @return array
+     */
+    public function keySet() {
+        return array_keys($this->_hashTable);
+    }
+    
+    /**
+     * Renseigner une paire de clef/valeur dans la map
+     * @param scalar $key clef
+     * @param mixed $value valeur
+     * @return void
+     */
+    public function put($key, $value) {
+        if(is_scalar($key)) {
+            if( !$this->_retainKeyCase) {
+                $key = strtolower($key);
+            }
+        }
+        $this->_hashTable[$key] = $value;
+    }
+
+    /**
+     * Combine les valeurs d'une HashMap avec cette HashMap
+     * @param HashMap $hash
+     */
+    public function putAll(HashMap $hash) {
+        foreach($hash as $key => $value) {
+            $this->put($key, $value);
+        }
+    }
+    
+    /**
+     * Supprime une association clef/valeur de la map
+     * @param scalar $key
+     */
+    public function remove($key) {
+        unset($this->_hashTable[$key]);
+    }
+
+    /**
+     * Retourne la taille de la HashMap
+     * @return int
+     */
+    public function size() {
+        return count($this->_hashTable);
+    }
+    
+    /**
+     * Retourne une representation string de la HashMap construite sous la forme
+     * [Nom de la classe]@[hash de la classe]
+     * @return string
+     */
+    public function __toString() {
+        return __CLASS__ . '@' . hash($this);
+    }
+    
+    /**
+     * Retourne une collection de valeurs (sans clefs)
+     * @return array
+     */
+    public function values() {
+        return array_values($this->_hashTable);
+    }
+
+    /**
+     * Return all element in map as an associated array. Same as HashMap::toArray()
+     *
+     * @return array
+     */
+    public function getAll() {
+        return $this->_hashTable;
+    }
+
+    /**
+     * Returns a string list of all elements in map. The end line paramter will be appended to the end
+     * of each key-value pair.
+     *
+     * @return String
+     * @param String $endLine[optional]
+     */
+    public function toString($endLine = "\n") {
+        $string = "";
+        foreach($this as $key => $value) {
+            if(is_object($value)) {
+                $string .= "[$key] : " . $value->toString() . $endLine;
+                continue;
+            }
+            $string .= "$key : $value $endLine";
+
+        }
+        return $string;
+    }
+
+    /**
+     * Ajouter tous les elements d'un tableau dans cette Map
+     * @param array $source
+     */
+    public function addArray($source) {
+        if(!is_array($source)) {
+            throw new Exception("Le parametre n'est pas un tableau");
+        }
+        foreach($source as $key => $value) {
+            $this->put($key, $value);
+        }
+    }
+
+    /* Iterator Methods: 
+    ==================================================================*/
+    public function rewind() {
+        $this->_valid = (FALSE !== reset($this->_hashTable));
+    }
+
+    /**
+     * Return the current array element
+     */
+    public function current() {
+        return current($this->_hashTable);
+    }
+
+    /**
+     * Return the key of the current array element
+     */
+
+    public function key() {
+        return key($this->_hashTable);
+    }
+
+    /**
+     * Move forward by one
+     * PHP's next() returns false if there are no more elements
+     */
+    public function next() {
+        $this->_valid = (FALSE !== next($this->_hashTable));
+    }
+
+    /**
+     * Is the current element valid?
+     */
+    public function valid() {
+        return $this->_valid;
+    }
+    /* Iterator Methods: 
+    ==================================================================*/
+
+}
+?>
